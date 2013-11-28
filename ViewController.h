09db0d7c716673349f243cf2a4139fb3853316ae
taneemdz <tanee.mdz@gmail.com>
2013-11-28 23:05:45 +0530

    Create ViewController.h

---
+++ ViewController.h
@@ -0,0 +1,11 @@
+
+
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UITextField *userInput;
+@property (weak, nonatomic) IBOutlet UITextField *password;
+- (IBAction)login:(id)sender;
+
+@end
